ab9f6f08d527a7c30c698daa9b35e2a98b14bda3
ecaracasg <34006770+elnnoncaracas@users.noreply.github.com>
2017-12-21 21:12:04 -0600

    otro cambio
    iniciando

---
+++ Blog/Blog/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Usuario calu = new Usuario();
+            calu.nombre = "chepe";
+            Console.WriteLine(calu.nombre);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Blog/Blog/Usuario.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog
+{
+    class Usuario
+    {
+        public int id;
+        public string nombre, apellido, email;
+
+        public Usuario(){}
+        public Usuario(int id, string nombre, string apellido, string email)
+        {
+            this.Nombre = nombre;
+            this.Apellido = apellido;
+            this.Email = email;
+            this.Id = id;
+        }
+
+        public int Id { get; }
+        public string Nombre { get; }
+        public string Apellido { get; }
+        public string Email { get; }
+        
+
+    }
+}
