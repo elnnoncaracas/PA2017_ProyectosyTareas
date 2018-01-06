86f19ebc5bb1fd2ff005874e34c038137bc81ce2
ecaracasg <34006770+elnnoncaracas@users.noreply.github.com>
2018-01-05 22:24:06 -0600

    AgregarListas
    Se agregaron listas tal y como se indico en la tarea.

---
+++ EjercicioPolimorfismoPA2017DOM/EjercicioPolimorfismoPA2017DOM/ListaDatos.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Listas
+{
+    class ListaDatos<T>
+    {
+        public List<T> lista { get; set; }
+        public ListaDatos()
+        {
+            this.lista = new List<T>();
+        }
+
+        public void Agregar(T item)
+        {
+            lista.Add(item);
+        }
+    }
+}
--- EjercicioPolimorfismoPA2017DOM/EjercicioPolimorfismoPA2017DOM/Program.cs
+++ EjercicioPolimorfismoPA2017DOM/EjercicioPolimorfismoPA2017DOM/Program.cs
@@ -4,12 +4,44 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EjercicioPolimorfismoPA2017DOM
+namespace Listas
 {
     class Program
     {
         static void Main(string[] args)
         {
+            ListaDatos<Estudiante> estudiantes = new ListaDatos<Estudiante>();
+            ListaDatos<Curso> cursos = new ListaDatos<Curso>();
+
+            for (int i = 1; i <= 5; i++)
+            {
+                Estudiante estudiante = new Estudiante();
+                estudiante.NombreEstudiante = "Estudiante " + i;
+
+                estudiantes.Agregar(estudiante);
+            }
+
+            for (int i = 1; i <= 5; i++)
+            {
+                Curso curso = new Curso();
+                curso.NombreCurso = "Curso " + i;
+
+                cursos.Agregar(curso);
+            }
+
+            // estudiantes.lista.ForEach(item => Console.WriteLine(item.NombreEstudiante));
+
+            foreach (var item in estudiantes.lista)
+            {
+                Console.WriteLine(item.NombreEstudiante);
+            }
+
+            foreach (var item in cursos.lista)
+            {
+                Console.WriteLine(item.NombreCurso);
+            }
+
+            Console.ReadKey();
         }
     }
 }
