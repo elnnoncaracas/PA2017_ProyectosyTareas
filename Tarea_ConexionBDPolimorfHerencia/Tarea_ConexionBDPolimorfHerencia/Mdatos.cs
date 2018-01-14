4820fb1ad2cdc83c5827aa800f925f29e35acfd8
ecaracasg <34006770+elnnoncaracas@users.noreply.github.com>
2018-01-13 22:30:32 -0600

    Casi_Listo
    Aplicado:
    +Herencia
    +Polimorfismo/Interfaces
    +ConexionBD Completa
    
    Missing:
    -Lista Generica

--- Tarea_ConexionBDPolimorfHerencia/Tarea_ConexionBDPolimorfHerencia/Mdatos.cs
+++ Tarea_ConexionBDPolimorfHerencia/Tarea_ConexionBDPolimorfHerencia/Mdatos.cs
@@ -10,12 +10,14 @@ namespace Tarea_ConexionBDPolimorfHerencia
 {
     class Mdatos:ListaString
     {
-        public string commPgSql;
+        public string commPgSql { get; set; }
 
         public void MdatosDB() {
-                
+            using (NpgsqlConnection con = new NpgsqlConnection(@"Server=baasu.db.elephantsql.com;Database=jrdkibpv;User Id=jrdkibpv;Password=65hxdEfRt-7GeWf3CnWw_5iI7Yx_wdRB;Port=5432"))
+            {
+                con.Open();
                 string commPgSql = "SELECT * FROM \"public\".\"spatial_ref_sys\" LIMIT 5";
-                NpgsqlCommand comando = new NpgsqlCommand(commPgSql);
+                NpgsqlCommand comando = new NpgsqlCommand(commPgSql,con);
                 NpgsqlDataReader dr = comando.ExecuteReader(); //Error - La conexion no esta abierta.
                 while (dr.Read())
                 {
@@ -24,11 +26,28 @@ namespace Tarea_ConexionBDPolimorfHerencia
 
                     Console.WriteLine("id: {0} - name: {1}", id, name);
                 }
+
+                con.Close();
+            }
             }
         public void MdatosDB(string commPgSql) {//A esta funcion aun no le he asignado nada.. 
-                                                    //la idea es que el usuario pueda introducir su propio Query de SQL
+            using (NpgsqlConnection con = new NpgsqlConnection(@"Server=baasu.db.elephantsql.com;Database=jrdkibpv;User Id=jrdkibpv;Password=65hxdEfRt-7GeWf3CnWw_5iI7Yx_wdRB;Port=5432"))
+            {
+                con.Open();
+                NpgsqlCommand comando = new NpgsqlCommand(commPgSql, con);
+                NpgsqlDataReader dr = comando.ExecuteReader(); //Error - La conexion no esta abierta.
+                while (dr.Read())
+                {
+                    int id = dr.GetInt32(0);
+                    string name = dr.GetString(1);
 
-            this.commPgSql = commPgSql;
+                    Console.WriteLine("id: {0} - name: {1}", id, name);
+                }
+
+                con.Close();
+            }                                       //la idea es que el usuario pueda introducir su propio Query de 
+            
+
             
         }
 
--- Tarea_ConexionBDPolimorfHerencia/Tarea_ConexionBDPolimorfHerencia/Program.cs
+++ Tarea_ConexionBDPolimorfHerencia/Tarea_ConexionBDPolimorfHerencia/Program.cs
@@ -10,15 +10,40 @@ namespace Tarea_ConexionBDPolimorfHerencia
     {
         static void Main(string[] args)
         {
-            ListaString lsi = new ListaString();
-            lsi.conectarBD();
-            Console.WriteLine("Presiona cualquier tecla para continuar");
-            Console.ReadKey();
-            Mdatos showdata = new Mdatos();
-            showdata.MdatosDB();
-            Console.WriteLine("Presiona cualquier tecla para continuar");
-            lsi.desconectarBD();
-            Console.ReadKey();
+            
+            Console.WriteLine("Hola, por favor selecciona una opcion: \n\n\n");
+            Console.WriteLine("\t\t\t1. Ejecutar Consulta SQL por Defecto");
+            Console.WriteLine("\t\t\t2. Escribir consulta SQL Personalizada");
+            int vswitch = Int32.Parse((Console.ReadLine()));
+            switch (vswitch) {
+                case 1:
+
+                        ListaString lsi = new ListaString();
+                        lsi.conectarBD();
+                        Console.WriteLine("Presiona cualquier tecla para continuar");
+                        Console.ReadKey();
+                        Mdatos showdata = new Mdatos();
+                        showdata.MdatosDB();
+                        Console.WriteLine("Presiona cualquier tecla para continuar");
+                        lsi.desconectarBD();
+                        Console.ReadKey();
+                    break;
+                case 2:
+                        ListaString lscarg = new ListaString();
+                        lscarg.conectarBD();
+                        Console.WriteLine("Presiona cualquier tecla para continuar");
+                        Console.ReadKey();
+                        Mdatos showdt = new Mdatos();
+                        Console.WriteLine("Ingresa tu consulta SQL _Postgre\n La unica tabla que se puede consultar es: \"public.spatial_ref_sys\"");
+                        string consulta = Console.ReadLine();
+                        showdt.MdatosDB(consulta);
+                        Console.ReadKey();
+                        lscarg.desconectarBD();
+                    break;
+                default:
+                    Console.WriteLine("Opcion no valida para este menú");
+                    break;
+        }
         }
     }
 }
